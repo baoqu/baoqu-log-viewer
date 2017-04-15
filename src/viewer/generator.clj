45fe88a575893d021eec11ccf44ee00a62a53bcf
Miguel de la Cruz <miguel@mcrx.me>
2017-04-15 13:56:25 +0100

    First approach to the functions related with the output generation

--- src/viewer/generator.clj
+++ src/viewer/generator.clj
@@ -1,6 +1,33 @@
 (ns viewer.generator
-  (:require [viewer.db :as db]))
+  (:require [hiccup.page :refer [html5]]
+            [viewer.db :as db]))
+
+(defn generate-template
+  "This function generates the HTML output mixing a predefined
+  template and the args it receive"
+  [{votes :votes ideas :ideas comments :comments}]
+  (html5 [:head
+          [:title "Hola mundo"]
+          [:meta {:charset "utf8"}]]
+         [:body
+          [:div [:h1 "IDEAS"]
+           [:ul
+            (for [{id :idea-id
+                   name :idea-name
+                   user :user} votes]
+              [:li name])]]]))
+
+(defn get-data-map
+  "This function gets all the needed data from the database and
+  returns it in a map"
+  [path]
+  {:votes (db/get-all-votes path)
+   :ideas (db/get-all-ideas path)
+   :comments (db/get-all-comments path)})
 
 (defn generate-log
   [path]
-  (println (map :body (db/get-all-comments path))))
+  (->> path
+       (get-data-map)
+       (generate-template)
+       (spit "/tmp/output.html")))
