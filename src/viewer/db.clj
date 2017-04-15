a754a001d0ff2007fdebb9085733f23c405d45c8
Miguel de la Cruz <miguel@mcrx.me>
2017-04-16 00:08:51 +0100

    Add some docstrings to the db namespace

--- src/viewer/db.clj
+++ src/viewer/db.clj
@@ -1,11 +1,16 @@
 (ns viewer.db
+  "This namespace contains all the functions used to extract data from
+  the `sqlite` database. The queries are read from the `queries.sql`
+  file in the `resources` folder, so they are isolated and easy to
+  modify directly in `SQL` without any other layer in between our
+  clojure code and it."
   (:require [hugsql.core :as hugsql]))
 
 (hugsql/def-db-fns "sql/queries.sql")
 
 (defn get-config
   "This method receives the path of the database file and returns a
-  map with its configuration ready to be used with `hugsql`"
+  map with its configuration ready to be used with `hugsql`."
   [path]
   {:classname "org.sqlite.JDBC"
    :subprotocol "sqlite"
@@ -13,7 +18,7 @@
 
 (defn run-query
   "We define a general function to run any query and then we define
-  partial applications of this function for any given case"
+  partial applications of this function for any given case."
   [query path]
   (-> path
       (get-config)
