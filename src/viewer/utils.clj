79af1adf9eb3ea404533745c4800d1b978575200
Miguel de la Cruz <miguel@mcrx.me>
2017-07-02 18:41:41 +0100

    Add get-base-dir function

--- src/viewer/utils.clj
+++ src/viewer/utils.clj
@@ -1,5 +1,6 @@
 (ns viewer.utils
-  "This namespace is used to hold some auxiliar functions.")
+  "This namespace is used to hold some auxiliar functions."
+  (:require [clojure.java.io :as io]))
 
 (defn path-exists?
   "This method receives a path and instantiates a
@@ -9,6 +10,19 @@
   (-> path
       (clojure.java.io/file)
       (.exists)))
+
+(defn get-base-dir
+  "This method returns the absolute path in the form of a string of a
+  given relative path. If the path is a directory, it returs just the
+  absolute path; if it's a file, it extracts the directory's path and
+  returns it."
+  [path]
+  (let [file (io/file path)]
+    (if (.isDirectory file)
+      (.getAbsolutePath file)
+      (-> file
+          (.getAbsoluteFile)
+          (.getParent)))))
 
 (defn usage
   "This method prints the usage string. It is used both by itself and
